4c2d645d6dd8612db831a97482191cbc802380b3
adwitafernanda <adwitafernanda4@gmail.com>
2023-07-02 17:15:59 +0700

    commit

--- BinarySearchTree_045/BinarySearchTree_045.cpp
+++ BinarySearchTree_045/BinarySearchTree_045.cpp
@@ -70,3 +70,18 @@ public:
 				currentNode = currentNode->rightchild;
 		}
 	} 
+
+	void inorder(node* ptr)
+	{
+		if (ROOT == NULL)
+		{
+			cout << "Tree is empty" << endl;
+			return;
+		}
+		if (ptr != NULL)
+		{
+			inorder(ptr->leftchild);
+			cout << ptr->info << "  ";
+			inorder(ptr->rightchild);
+		}
+	}
